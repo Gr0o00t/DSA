6b03c40d17ec89d66370993941ff89de7136b173
ArpitGoenka <2001arpitgoenka@gmail.com>
2024-06-02 02:37:10 +0300

    Basic mathmatics problems

--- Mathematics/prime.py
+++ Mathematics/prime.py
@@ -10,7 +10,40 @@ def isPrime_naive(n):
 
     return True
 
-def
+
+# TIme Complexity = bif O( square root of n)
+def isPrime_Divisors(num1):
+    if num1 == 1:
+        return False
+
+    i=2
+    while (i*i<=num1):
+        if num1%i==0:
+            return False
+        i+=1
+    return True
+
+
+# Time Complexity is 3 times faster than previous approach
+def isPrime(n):
+
+    if n == 1:
+        return False
+
+    if n == 2 or n ==3:
+        return True
+
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i=5
+
+    while (i*i <= n):
+        if n % i == 0 or n %(i+2) == 0:
+            return False
+        i+=6
+
+    return True
 
 if __name__ == "__main__":
     print(isPrime(53))
